7de536a4103f8e4ba882a94b74c4a23fe3092a11
kyklospi <saya.atikaa@hotmail.com>
2018-10-02 14:14:09 +0200

    Update TestTotalCoverageCounter.java

--- TestTotalCoverageCounter.java
+++ TestTotalCoverageCounter.java
@@ -1,21 +1,41 @@
-package com.bonial.api.destinations.test;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 /**
- * Created by atika.rachmawati on 13.09.18.
- * <p>
  * Total test coverage data as sum of collected coverage from jacoco
  */
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-public class TestTotalCoverageCounter {
+class TestTotalCoverageCounter {
     private float instruction;
     private float branch;
     private float line;
     private float complexity;
     private float method;
+    
+    TestTotalCoverageCounter() {
+    }
+
+    TestTotalCoverageCounter(float instruction, float branch, float line, float complexity, float method) {
+        this.instruction = instruction;
+        this.branch = branch;
+        this.line = line;
+        this.complexity = complexity;
+        this.method = method;
+    }
+
+    float getInstruction() {
+        return instruction;
+    }
+
+    float getBranch() {
+        return branch;
+    }
+
+    float getLine() {
+        return line;
+    }
+
+    float getComplexity() {
+        return complexity;
+    }
+
+    float getMethod() {
+        return method;
+    }
 }
