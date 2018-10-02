dfceafd9cc9b00c59191a736d74e5a2f8880d865
kyklospi <saya.atikaa@hotmail.com>
2018-10-02 14:08:42 +0200

    Update TestCoverageCounter.java

--- TestCoverageCounter.java
+++ TestCoverageCounter.java
@@ -1,16 +1,7 @@
-package com.bonial.api.destinations.test;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 /**
- * Created by atika.rachmawati on 13.09.18.
- * <p>
  * Test coverage data read from jacoco report
  */
-@AllArgsConstructor
-@Getter
-public class TestCoverageCounter {
+class TestCoverageCounter {
     private String group;
     private String packageName;
     private String className;
@@ -24,4 +15,77 @@ public class TestCoverageCounter {
     private int complexityCovered;
     private int methodMissed;
     private int methodCovered;
+    
+    TestCoverageCounter(String group, String packageName, String className, 
+                               int instructionMissed, int instructionCovered, 
+                               int branchMissed, int branchCovered, 
+                               int lineMissed, int lineCovered, 
+                               int complexityMissed, int complexityCovered, 
+                               int methodMissed, int methodCovered) {
+        this.group = group;
+        this.packageName = packageName;
+        this.className = className;
+        this.instructionMissed = instructionMissed;
+        this.instructionCovered = instructionCovered;
+        this.branchMissed = branchMissed;
+        this.branchCovered = branchCovered;
+        this.lineMissed = lineMissed;
+        this.lineCovered = lineCovered;
+        this.complexityMissed = complexityMissed;
+        this.complexityCovered = complexityCovered;
+        this.methodMissed = methodMissed;
+        this.methodCovered = methodCovered;
+    }
+    
+    String getGroup() {
+        return group;
+    }
+
+    String getPackageName() {
+        return packageName;
+    }
+
+    String getClassName() {
+        return className;
+    }
+
+    int getInstructionMissed() {
+        return instructionMissed;
+    }
+
+    int getInstructionCovered() {
+        return instructionCovered;
+    }
+
+    int getBranchMissed() {
+        return branchMissed;
+    }
+
+    int getBranchCovered() {
+        return branchCovered;
+    }
+
+    int getLineMissed() {
+        return lineMissed;
+    }
+
+    int getLineCovered() {
+        return lineCovered;
+    }
+
+    int getComplexityMissed() {
+        return complexityMissed;
+    }
+
+    int getComplexityCovered() {
+        return complexityCovered;
+    }
+
+    int getMethodMissed() {
+        return methodMissed;
+    }
+
+    int getMethodCovered() {
+        return methodCovered;
+    }
 }
